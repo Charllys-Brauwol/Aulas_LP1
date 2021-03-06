f35311842e1eb73b81665f8b65d917a32044cfd4
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-06 15:43:24 -0300

    exercicios

--- a02ex03.py
+++ a02ex03.py
@@ -1,10 +1,10 @@
 '''3 - Crie uma função que retorne o resto da divisão
 do resultado da função da questão anterior por 2'''
 
-def exerc02 ():
+number_one = int(input('Digite o primeiro número:'))
+number_two = int(input('Digite o segundo número:'))
 
-    number_one = int(input('Digite o primeiro número:'))
-    number_two = int(input('Digite o segundo número:'))
+def exerc02 (number_one, number_two):
     
     result = ((number_one*2)*(number_two*3))
     
@@ -12,7 +12,7 @@ def exerc02 ():
 
     return result
 
-def division (result):
-    value = result % 2
 
-    print (f'O valor é: {value}.')
+result = exerc02 (number_one, number_two)
+value = result % 2
+print (f'O valor é: {value}.')

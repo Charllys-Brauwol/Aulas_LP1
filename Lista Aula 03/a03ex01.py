516ba0f9f8f4485ee546d569f1616b3544bb9a90
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-07 16:22:18 -0300

    exercicios

---
+++ Lista Aula 03/a03ex01.py
@@ -0,0 +1,5 @@
+'''1 - Escreva uma função que simule o funcionamento de um radar eletrônico.
+Essa função deve receber a velocidade do carro de um usuário. Caso ultrapasse 80 Km/h,
+exiba uma mensagem dizendo que o usuário foi multado. Nesse caso, exiba o valor da multa,
+cobrando R$ 90 reais pela infração + R$ 5 reais por km acima de 80 km/h'''
+

da5b2a0c1e9feea3034c1ad92d084509e3cf2348
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 19:39:19 -0300

    Lista 02 - Q 14

--- Lista Aula 02/a02ex14.py
+++ Lista Aula 02/a02ex14.py
@@ -2,12 +2,15 @@
 Escreva uma função que receba a quantidade de cada animal
 que o fazendeiro possui atualmente e retorne quantas patas tem na fazenda.'''
 
+#Input recebendo os dados do tipo inteiro
 chickens = int(input('Quantas galinhas você têm?'))
 cows = int(input('Quantas vacas você têm?'))
 pigs = int(input('Quantas porcos você têm?'))
 
+#Função com retorno do total de patas
 def total_paws(chickens, cows, pigs):
     total = ((chickens * 2) + ((cows + pigs) * 4))
     return total
 
+#Print do valor que a função retorna
 print(f'O total de patas é:\n{total_paws(chickens, cows, pigs)}')

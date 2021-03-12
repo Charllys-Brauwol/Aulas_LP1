2c018b795cb3da575b6b3f3b99669076b829344f
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 19:49:11 -0300

    Lista 02 - Q 03

--- Lista Aula 02/a02ex03.py
+++ Lista Aula 02/a02ex03.py
@@ -4,6 +4,7 @@ do resultado da função da questão anterior por 2'''
 number_one = int(input('Digite o primeiro número:\n'))
 number_two = int(input('Digite o segundo número:\n'))
 
+#Função da questão 02
 def exerc02 (number_one, number_two):
     
     result = ((number_one*2)*(number_two*3))
@@ -12,7 +13,11 @@ def exerc02 (number_one, number_two):
 
     return result
 
+#Função de retorno da divisão
+def total (exerc02):
+    result = exerc02 (number_one, number_two)
+    value = result % 2
+    return (value)
 
-result = exerc02 (number_one, number_two)
-value = result % 2
-print (f'O resto dadivisão por 2 é: \n{value}.')
+#Print do valor
+print (f'O resto dadivisão por 2 é: \n{total(exerc02)}.')

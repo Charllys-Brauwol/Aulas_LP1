0b2675cca278c80351e7ba4987ff7264a6f108b4
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 20:08:19 -0300

    Lista 02 - Q 12

--- Lista Aula 02/a02ex12.py
+++ Lista Aula 02/a02ex12.py
@@ -3,11 +3,19 @@ calcule a área e perímetro de uma circuferência.'''
 
 print('Vamos calcular a área de uma circuferência:')
 
+#Input recebendo os dados
 raio = float(input('Informe o raio da circunferência:\n'))
 
+#Funçao de calculo
 def perimeter_calculation(raio):
-    area_value = float(3.14 * (raio ** 2))
-    print(f'A área é igual: {area_value}%.2f')
-    perimeter = float(2 * 3.14 * raio)
-    print(f'O perímetro é igual: {perimeter}%.2f')
-    return area_value, perimeter
+    return (float(2 * 3.14 * raio))
+
+print(f'O perímetro é igual: {perimeter_calculation(raio):.2f}')
+
+def area_calculation(raio):
+    return (float(3.14 * (raio ** 2)))
+
+print(f'A área é igual: {area_calculation(raio):.2f}')
+'''
+print(f'A área é igual: {perimeter_calculation(raio):.2f}')
+print(f'O perímetro é igual: {perimeter:.2f}')'''

4d535166989809d3eb7f41aabeee805099ef3499
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 19:54:13 -0300

    Lista 02 - Q 09

--- Lista Aula 02/a02ex09.py
+++ Lista Aula 02/a02ex09.py
@@ -4,16 +4,21 @@ e retorne False caso contrário.
 Pesquise pelo método len() na documentação do Python.'''
 
 print('Digite duas palavras e vamos ver se possuem a mesma quantidade de letras:')
+
+#Input que receberá as duas strings
 word_one = input('Digite a primeira palavra:\n')
 word_two = input('Digite a segunda palavra:\n')
 
+#Método len, que irá calcular a quantidade de letras de cada string
 first_quantity = len(word_one)
 second_quantity = len(word_two)
 
+#Função de comparação
 def compare_words(first_quantity, second_quantity):
     if (first_quantity == second_quantity):
         return True
     else:
         return False
 
+#Print do resultado
 print(f'O retorno foi: {compare_words(first_quantity, second_quantity)}')

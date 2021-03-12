be2b6b5cd7247926a1acba03c2b645f65668df11
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 19:50:44 -0300

    Lista 02 - Q 05

--- Lista Aula 02/a02ex05.py
+++ Lista Aula 02/a02ex05.py
@@ -4,14 +4,16 @@ assim como a quantidade de dia pelos quais o carro foi alugado.
 A função deve retornar o preço a pagar,
 sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.'''
 
+#Input recebendo os valores do tipo inteiro
 number_of_days = int(input('Quantos dias você andou com o carro?\n'))
 number_of_km = int(input('Quantos km você andou com o carro?\n'))
 
 
-
+#Função que retorna o total a pagar
 def pay (number_of_days, number_of_km):
     total_payment = ((number_of_days * 60.00) + (number_of_km * 0.15))
 
     return (total_payment)
 
+#Print do valor
 print (f'Total que irá pagar: \n{pay(number_of_days, number_of_km):.2f}')

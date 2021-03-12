be20facc2b5137959f62dc4a1ae4a1622474553a
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 19:45:52 -0300

    Lista 02 - Q 02

--- Lista Aula 02/a02ex02.py
+++ Lista Aula 02/a02ex02.py
@@ -1,10 +1,13 @@
 '''2 - Escreva uma função que receba dois números como argumento
  e retorne o produto do dobro do primeiro pelo triplo do segundo'''
 
+#Input recebedo os números do tipo inteiro
 number_one = int(input('Digite o primeiro número:\n'))
 number_two = int(input('Digite o segundo número:\n'))
 
-result = ((number_one*2)*(number_two*3))
+#Função do calculo 
+def product(number_one, number_two):
+    return ((number_one*2)*(number_two*3))
 
-print (f'O valor é: \n{result}.')
+print (f'O valor é: \n{product(number_one, number_two)}.')
  

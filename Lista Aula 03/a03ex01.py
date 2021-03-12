7acc6ad6e2d444087226f6f7b1c7dd89aef80bdf
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 20:45:25 -0300

    Lista 03 - Q 01

--- Lista Aula 03/a03ex01.py
+++ Lista Aula 03/a03ex01.py
@@ -1,7 +1,9 @@
-'''1 - Escreva uma função que simule o funcionamento de um radar eletrônico.
+'''
+1 - Escreva uma função que simule o funcionamento de um radar eletrônico.
 Essa função deve receber a velocidade do carro de um usuário. Caso ultrapasse 80 Km/h,
 exiba uma mensagem dizendo que o usuário foi multado. Nesse caso, exiba o valor da multa,
-cobrando R$ 90 reais pela infração + R$ 5 reais por km acima de 80 km/h'''
+cobrando R$ 90 reais pela infração + R$ 5 reais por km acima de 80 km/h
+'''
 
 velocity = int(input('Qual a velocidade que o carro estava em km/h?\n'))
 

eed5c1b960b2a398f002cbee9eb668272fdc40f6
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 20:38:03 -0300

    Lista 02 - Q 07

--- Lista Aula 02/a02ex07.py
+++ Lista Aula 02/a02ex07.py
@@ -1,9 +1,14 @@
 '''7 - Escreva uma função que receba a idade do usuário e indique 
 se ele pode ou não encher a cara de cachaça.'''
 
+#input recebedo a idade
 age = int(input('Vamos saber se você já pode beber.\nQual sua idade?\n'))
 
-if (age >= 18):
-    print ('Parabéns! Você pode escolher.')
-else:
-    print ('Não ainda.')
+def age_drink(age):
+    if age >= 18:
+        print ('Parabéns! Você pode escolher.')
+    else:
+        print ('Não ainda.')
+    return
+
+x = age_drink (age)

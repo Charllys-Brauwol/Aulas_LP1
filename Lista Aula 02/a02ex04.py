a10ef8f09fbd932032f15d10bbae45ea5d0eb5c4
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 20:23:53 -0300

    Lista 02 - Q 04

--- Lista Aula 02/a02ex04.py
+++ Lista Aula 02/a02ex04.py
@@ -4,15 +4,18 @@ Considere que um fumante perde 10 minutos de vida a cada cigarro,
 a função deverá retornar quantos dias de vida um fumante perderá.
 Exiba o total em dias.'''
 
+#Input recebendo os dados
 quantity_cigarettes = float(input('Quantos cigarros você fuma por dia?\n'))
 quantity_years = float(input('Quantos anos faz que você fuma?\n'))
 print('\n')
 
+#Função que retorna os valores perdidos.
 def lost_minutes (quantity_cigarettes, quantity_years):
     return (quantity_cigarettes * 10 * quantity_years * 360)
 
 amount = lost_minutes(quantity_cigarettes, quantity_years)
 
-amount = amount / 60 / 24
+#Variável converter em dias
+amount = amount / 1440
 
-print (f'O total de dias perdidos são: \n{amount:.0f} dias.')
+print (f'O total de dias perdidos são: \n{amount:.1f} dias.')

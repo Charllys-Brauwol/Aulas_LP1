f8d76ae553859e12dc5a58484c7be9c77e499314
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 19:55:40 -0300

    Lista 02 - Q 11

--- Lista Aula 02/a02ex11.py
+++ Lista Aula 02/a02ex11.py
@@ -3,18 +3,24 @@ calcule sua área e perímetro.'''
 
 print('Vamos calcular o Perímetro e a Área de uma Triângulo:')
 
+#Recebendo as medidas do triângulo
 one_side = float(input('Digite o tamanho do primeiro lado do triângulo:\n'))
 two_side = float(input('Digite o tamanho do segundo lado do triângulo:\n'))
 three_side = float(input('Digite o tamanho do terceiro lado do triângulo:\n'))
 base = float(input('Digite o tamanho da base do triângulo:\n'))
 height = float(input('Digite o tamanho da altura do triângulo:\n'))
 
+
+#Função do Perímetro
 def perimeter(one_side, two_side, three_side):
     return (one_side + two_side + three_side)
 
+#Print do perímetro
 print(f'O perímetro é: {perimeter(one_side, two_side, three_side)}')
 
+#Função da Área
 def area(base, height):
     return ((base * height) / 2)
 
+#Print da área
 print(f'A área é: {area(base, height)}')

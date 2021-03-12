44ba5430ce0d0693231f3fd0d983acf3a62c553f
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 20:45:04 -0300

    Lista 03 - Q 03

--- Lista Aula 03/a03ex03.py
+++ Lista Aula 03/a03ex03.py
@@ -1,6 +1,8 @@
-'''3 - Escreva um programa que pergunte a distância que um passageiro deseja percorrer em km. 
+'''
+3 - Escreva um programa que pergunte a distância que um passageiro deseja percorrer em km. 
 Calcule o preço da passagem, 
-cobrando R$ 0.50 por km para viagens de até 200 km, e R$ 0.45 para viagens mais longas.'''
+cobrando R$ 0.50 por km para viagens de até 200 km, e R$ 0.45 para viagens mais longas.
+'''
 
 km = float(input('Quantos km você vai percorrer?\n'))
 

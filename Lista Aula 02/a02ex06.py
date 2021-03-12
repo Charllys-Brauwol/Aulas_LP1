f448a3fd86bbaf89a0fab3cabcd8cdce20c0fa81
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-12 19:52:06 -0300

    Lista 02 - Q 06

--- Lista Aula 02/a02ex06.py
+++ Lista Aula 02/a02ex06.py
@@ -3,12 +3,14 @@ Mas as livrarias recebem um desconto de 40%.
 O transporte custa 3.00 para o primeiro exemplar e 75 centavos para cada exemplar adicional. 
 Qual é o custo total de atacado para 60 copias?'''
 
+#Valores declarados
 book_cover = (24.95 * 0.4)
 transport = (3.00 + (60*0.75))
 
-
+#Função de calculo do valor total
 def total_price (book_cover, transport):
     price = book_cover + transport
     return (price)
 
+#Print da informação do valor dos 60 livros
 print (f'O valor total dos 60 livros é igual: \n{total_price (book_cover, transport):.2f}')

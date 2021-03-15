30f151670f32927e2a0d65889ad5cb9f7250e0c7
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-15 19:12:26 -0300

    Aula 04 - Q 04

---
+++ Lista Aula 04/a04exe04.py
@@ -0,0 +1,10 @@
+'''4 - Crie uma função que verifique a existência de um número apocalíptico.
+Um número é apocalíptico se dentro dele tiver a sequência 666.
+Sua função deve receber um número que será a potência de base 2.
+Se o número obtido for apocalíptico retorne a seguinte mensagem:
+"Arrependam-se vocês têm x dias até o fim", sendo 'x' a quantidade de dias,
+determinada pela posição(indice ) onde estava a sequência 666.
+Caso o número não seja apocalíptico, retorne a seguinte mensagem: "Está tudo bem galera,
+pode continuar pecando".
+
+Dica: Procure na documentação pelos métodos find() e index().'''

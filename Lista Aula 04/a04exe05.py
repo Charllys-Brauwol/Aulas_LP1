7d8238095b7a3ed393e7a5c77fad97cad14b8a14
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-20 21:06:28 -0300

    Aula 04 - Q 05

--- Lista Aula 04/a04exe05.py
+++ Lista Aula 04/a04exe05.py
@@ -3,3 +3,14 @@ Ela deve receber como argumento, uma string, o número minimo e máximo de
 caracteres e retornar VERDADEIRO se o número de caracteres da string 
 estiver dentro do min e max passado, e FALSO caso contrário.'''
 
+string = input('Digite uma frase entre 5 e 10 letras:\n')
+
+quantidade = len(string)
+
+def validar(string):
+    if quantidade >= 5 and quantidade <=10:
+        print(True)
+    else:
+        print(False)
+
+validar(string)

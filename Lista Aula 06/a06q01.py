09a0565aee101f6e48bb3e8b100f3f5267af3a9d
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-21 15:31:39 -0300

    Aula 06 - Q 01

---
+++ Lista Aula 06/a06q01.py
@@ -0,0 +1,11 @@
+'''
+1 - Faça uma função que receba duas listas e que gere uma terceira com os elementos das duas primeiras.
+'''
+
+lista_01 = [1, 2, 3, 4]
+lista_02 = [5, 6, 7, 8]
+
+def unir(lista_01, lista_02):
+    return lista_01+lista_02
+
+print(unir(lista_01, lista_02))

143d096daa7c4883a1abd9503617da25a74835b7
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-03-27 17:01:32 -0300

    Aula 02 - Q 08

--- Lista Aula 02/a02ex08.py
+++ Lista Aula 02/a02ex08.py
@@ -3,3 +3,9 @@ Aplique De Morgan na seguinte proposição e atribua o valor a uma variável
  ~(p ^ (p v q)), essa variável deve ser retornada partir de uma função'''
 
 
+def morgan(P, Q):
+    return (not(P or (P and Q)))
+    
+x= morgan(True,False)
+print(x)
+
